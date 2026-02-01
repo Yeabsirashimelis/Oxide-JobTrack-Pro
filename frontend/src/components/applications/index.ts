eb953e5b39a944ff001091827eb750a56f3caac7
Yeabsirashimelis <shimelisyeabsiragithub@gmail.com>
2026-02-01 11:28:57 +0300

    feat: add applications page with Kanban pipeline view

---
+++ frontend/src/components/applications/index.ts
@@ -0,0 +1,5 @@
+export { default as JobCard } from "./JobCard";
+export { default as KanbanColumn } from "./KanbanColumn";
+export { default as KanbanBoard } from "./KanbanBoard";
+export type { Job } from "./JobCard";
+export type { PipelineStage } from "./KanbanColumn";
